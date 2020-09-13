760a753f0d174fe3d8014756ebf29ba53b47a6d1
dudwns9416 <dudwns9416@naver.com>
2020-09-13 14:27:06 +0900

    [NODE-03]npm install mongoose --save

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://@.bvkft.mongodb.net/?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
+
+
 app.get('/', (req, res) => {
     res.send('안녕 세상아!')
 })
